83d0640a249282c7e6db54f066ab2d443ed95b03
kaushikalex <106651493+kaushikalex@users.noreply.github.com>
2022-09-10 19:57:14 +0530

    Add files via upload

---
+++ StringPalindromeHE.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class StringPalindromeHE {
+    public static void main(String args[] ) throws Exception {
+
+        String s,x;
+        int n;
+        Scanner sc = new Scanner(System.in);
+        s=sc.next();
+        x=s;
+        n=s.length();
+        int i=n-1;
+        int j=0,ct=1;
+        while(i>=0 && j<n)
+        {
+            if(x.charAt(i)!=s.charAt(j))
+                ct=0;
+            i--;
+            j++;
+        }
+        if(ct==1)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+
+    }
+}
